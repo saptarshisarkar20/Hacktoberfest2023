7a873938150f2dd8bedf33fa62cd19d676f040fa
Venom <93427284+Mihir2423@users.noreply.github.com>
2022-10-08 19:35:45 +0530

    Add files via upload

---
+++ Pull Here/LeetCode/Palindrome-Partions/program.py
@@ -0,0 +1,26 @@
+def partition(s):
+    res = []
+    part = []
+    
+    def dfs(i):
+        if i>=len(s):
+            res.append(part.copy())
+            return
+        for j in range(i, len(s)):
+            if isPali(s, i, j) == True:
+                part.append(s[i:j+1])
+                dfs(j + 1)
+                part.pop()
+    dfs(0)
+    return res
+
+def isPali(s, l, r):
+    while l < r:
+        if s[l] != s[r]:
+            return False 
+        l, r = l + 1, r - 1
+    return True 
+
+string = "geek"
+a = list(string)
+print(partition(a))
