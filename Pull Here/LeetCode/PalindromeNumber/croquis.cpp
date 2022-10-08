85078a35ef0e112bce2fd6a13687b4e66478e5bb
Srimon Ghosh <ghoshsrimon@gmail.com>
2022-10-08 21:01:22 +0530

    cpp solution by croquis

---
+++ Pull Here/LeetCode/PalindromeNumber/croquis.cpp
@@ -0,0 +1,14 @@
+
+class PalindromeNumber {
+public:
+    bool main(int x) {
+        if(x<0|| (x!=0 &&x%10==0)) return false;
+        int sum=0;
+        while(x>sum)
+        {
+            sum = sum*10+x%10;
+            x = x/10;
+        }
+        return (x==sum)||(x==sum/10);
+    }
+};
