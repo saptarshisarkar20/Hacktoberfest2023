768b346c1a0b450740240c5e498e210955af89b3
mdsarfarazalam840 <41156047+mdsarfarazalam840@users.noreply.github.com>
2023-10-04 18:05:15 +0530

    Create mdsarfarazalam840.java

---
+++ Pull Here/LeetCode/CountingBits/mdsarfarazalam840.java
@@ -0,0 +1,11 @@
+//Added solution with O(n) complexity
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+        return ans;
+    }
+}
